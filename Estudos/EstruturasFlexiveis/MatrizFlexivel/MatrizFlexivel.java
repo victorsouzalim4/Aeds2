b5c5c9ad838ba5f95056e165f0c89e1136c3b2fe
Victor Souza Lima <you@example.com>
2024-05-13 19:47:47 -0300

    Implement Flexible Matrix with Linked Cell Structure
    - Added `Celula` class to represent each cell in the matrix, containing an integer element and pointers to adjacent cells (left, right, above, below).
    - Implemented `Matriz` class to manage a dynamic matrix:
      - Constructor initializes the matrix with 2 rows and 2 columns.
      - Methods `createLinha()` and `createLinha(int x)` to create a row of cells.
      - Method `createColuna(int x)` to create a column of cells.
      - Method `addLinha()` to add a new row at the bottom of the matrix.
      - Method `addColuna()` to add a new column to the right of the matrix.
      - Method `mostra()` to display the current state of the matrix in the console.

---
+++ Estudos/EstruturasFlexiveis/MatrizFlexivel/MatrizFlexivel.java
@@ -0,0 +1,130 @@
+package Estudos.EstruturasFlexiveis.MatrizFlexivel;
+
+class Celula{
+    public int elemento;
+    public Celula esq;
+    public Celula dir;
+    public Celula sup;
+    public Celula inf;
+
+    Celula(){
+        this.elemento = 0;
+        this.esq = this.dir = this.sup = this.inf = null;
+    }
+}
+
+class Matriz{
+    private Celula inicio;
+    private int NumLinhas;
+    private int NumColunas;
+
+    Matriz(){
+        inicio = createLinha();
+        Celula linhaAbaixo = createLinha();
+
+        for(Celula i = inicio, tmp = linhaAbaixo; i != null; i = i.dir){
+            i.inf = tmp;
+            tmp.sup = i;
+
+            tmp = tmp.dir;
+        }
+        NumLinhas = 2;
+        NumColunas = 2;
+
+    }
+
+    Celula createLinha(){
+        Celula i = new Celula();
+        Celula j = new Celula();
+
+        i.dir = j;
+        j.esq = i;
+
+        return i;
+    }
+
+    Celula createLinha(int x){
+        Celula i = new Celula();
+        Celula tmp = i;
+
+        for(int j = 0; j < x - 1; j++){
+            Celula nova = new Celula();
+            tmp.dir = nova;
+            nova.esq = tmp;
+            tmp = nova;
+        }
+
+        return i;
+    }
+
+    Celula createColuna(int x){
+        Celula i = new Celula();
+        Celula tmp = i;
+
+        for(int j = 0; j < x -1; j++){
+            Celula nova = new Celula();
+            tmp.inf = nova;
+            nova.sup = tmp;
+
+            tmp = nova;
+        }
+
+        return i;
+    }
+
+    void addLinha(){
+        Celula i;
+        for(i = inicio; i.inf != null; i = i.inf);
+        for(Celula j = createLinha(NumColunas); j != null; j = j.dir){
+            i.inf = j;
+            j.sup = i;
+
+            i = i.dir;
+        }
+        NumLinhas++;
+    }
+
+    void addColuna(){
+        Celula i;
+        for(i = inicio; i.dir != null; i = i.dir);
+        for(Celula j = createColuna(NumLinhas); j != null; j = j.inf){
+            i.dir = j;
+            j.esq = i;
+
+            i = i.inf;
+        }
+
+        NumColunas++;
+    }
+
+    void mostra(){
+        for(Celula i = inicio; i != null; i = i.inf){
+            for(Celula j = i; j != null; j = j.dir){
+                System.out.print(j.elemento + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
+public class MatrizFlexivel {
+    public static void main(String args[]){
+
+        Matriz mat = new Matriz();
+        mat.mostra();
+
+        mat.addLinha();
+        mat.mostra();
+
+        mat.addColuna();
+        mat.mostra();
+
+        mat.addColuna();
+        mat.mostra();
+
+        mat.addLinha();
+        mat.mostra();
+        
+    }
+}
