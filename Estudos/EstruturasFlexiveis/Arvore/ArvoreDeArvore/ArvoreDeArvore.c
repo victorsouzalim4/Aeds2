df788fbe4c0194029432e22e33dd5200dc0f6e20
VictorSouzaLima <victorsouzalima2064@gmail.com>
2024-05-18 20:30:46 -0300

    implementation of a method that inserts a name at the tree of trees and another method that prints all the names in the tree

--- Estudos/EstruturasFlexiveis/Arvore/ArvoreDeArvore/ArvoreDeArvore.c
+++ Estudos/EstruturasFlexiveis/Arvore/ArvoreDeArvore/ArvoreDeArvore.c
@@ -17,9 +17,6 @@ NoFilho* construtorNoFilho(char* nome){
     return i;
 }
 
-
-
-
 //---------------------------------------------
 
 typedef struct No{
@@ -33,9 +30,12 @@ No* construtorNo(char x){
     No* i = (No*) malloc(sizeof(No));
     i->elemento = x;
     i->dir = i->esq = NULL;
+    i->no = NULL;
 
     return i;
 }
+
+//-----------------------------------------------------------
 
 typedef struct Arvore{
     No* raiz;
@@ -47,6 +47,9 @@ Arvore* arvoreConstrutor(){
 
     return i;
 }
+
+//-----------------------------------------------------------
+
 
 No* inserir(No* i, char x){
     if(i == NULL){
@@ -69,6 +72,63 @@ void caminharCentral(No* i){
     }
 }
 
+NoFilho** getPointerPrimeiraLetraDoNome(No* i, char letra){
+    NoFilho** flag;
+    if(i == NULL){
+        printf("Letra nao encontrada");
+        flag = NULL;
+    }else if(letra > i->elemento){
+        flag = getPointerPrimeiraLetraDoNome(i->dir, letra);
+    }else if(letra < i->elemento){
+        flag = getPointerPrimeiraLetraDoNome(i->esq, letra);
+    }else{
+        flag = &(i->no);
+    }
+
+    return flag;
+}
+
+NoFilho* inserirNome(NoFilho* i, char* nome){
+    if(i == NULL){
+        i = construtorNoFilho(nome);
+    }else if(strcmp(nome, i->nome) > 0){
+        i->dir = inserirNome(i->dir, nome);
+    }else if(strcmp(nome, i->nome) < 0){
+        i->esq = inserirNome(i->esq, nome);
+    }else{
+        printf("ERRO, NOME INVALIDO\n");
+    }
+
+    return i;
+}
+
+void insereNome(No* i, char* nome){
+
+    NoFilho** j = getPointerPrimeiraLetraDoNome(i, nome[0]);
+    if(j != NULL){
+        *j = inserirNome(*j, nome);
+    }
+
+}
+
+void listaNomesDoNo(NoFilho* i){
+    if(i != NULL){
+        listaNomesDoNo(i->esq);
+        printf("%s\n", i->nome);
+        listaNomesDoNo(i->dir);
+    }
+}
+
+void mostrarNomes(No* i){
+    if(i != NULL){
+        mostrarNomes(i->esq);
+        listaNomesDoNo(i->no);
+        mostrarNomes(i->dir);
+    }
+}
+
+
+
 
 
 
@@ -77,18 +137,55 @@ int main(){
     Arvore* tree = arvoreConstrutor();
 
     tree->raiz = inserir(tree->raiz, 'V');
-
+    tree->raiz = inserir(tree->raiz, 'F');
+    tree->raiz = inserir(tree->raiz, 'A');
     tree->raiz = inserir(tree->raiz, 'I');
-
-    tree->raiz = inserir(tree->raiz, 'D');
-
+    tree->raiz = inserir(tree->raiz, 'W');
     tree->raiz = inserir(tree->raiz, 'E');
 
-    tree->raiz = inserir(tree->raiz, 'W');
+
     caminharCentral(tree->raiz);
     printf("\n");
 
-    NoFilho* test = construtorNoFilho("Victor");
-    printf("%s", test->nome);
+// Para 'V'
+insereNome(tree->raiz, "Victor");
+insereNome(tree->raiz, "Vanessa");
+insereNome(tree->raiz, "Vanusa");
+insereNome(tree->raiz, "Vinicius");
+
+// Para 'F'
+insereNome(tree->raiz, "Fernando");
+insereNome(tree->raiz, "Fabiana");
+insereNome(tree->raiz, "Felipe");
+insereNome(tree->raiz, "Francisco");
+
+// Para 'A'
+insereNome(tree->raiz, "Ana");
+insereNome(tree->raiz, "Alice");
+insereNome(tree->raiz, "Andre");
+insereNome(tree->raiz, "Antonio");
+
+// Para 'I'
+insereNome(tree->raiz, "Igor");
+insereNome(tree->raiz, "Isabela");
+insereNome(tree->raiz, "Ivan");
+insereNome(tree->raiz, "Ines");
+
+// Para 'W'
+insereNome(tree->raiz, "Wagner");
+insereNome(tree->raiz, "Wesley");
+insereNome(tree->raiz, "William");
+insereNome(tree->raiz, "Wanda");
+
+// Para 'E'
+insereNome(tree->raiz, "Eduardo");
+insereNome(tree->raiz, "Elaine");
+insereNome(tree->raiz, "Eliana");
+insereNome(tree->raiz, "Emanuel");
+
+
+   // printf("%s", tree->raiz->no->esq->dir->nome);
+
+   mostrarNomes(tree->raiz);
 
 }
